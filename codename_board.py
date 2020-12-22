d62acd2fec9ada641bc540d051f39009b60202a3
Markus Amano <markus.a.amano@gmail.com>
2020-12-22 15:56:50 -0700

    added thee red blue gray and black squares to the spaymaster_view

--- codename_board.py
+++ codename_board.py
@@ -7,4 +7,16 @@ class Board():
         The initializer of the codename board. It constructs the word_view which 
         is visible to all players and the spymaster_view. The board is a 5 by 5 board with 25 tiles.
         """
-        pass
+        self.spymaster_view = []
+
+        for _ in range(red):
+            self.spymaster_view.append("red")
+
+        for _ in range(blue):
+            self.spymaster_view.append("blue")
+
+        for _ in range(gray):
+            self.spymaster_view.append("gray")
+
+        for _ in range(black):
+            self.spymaster_view.append("black")
