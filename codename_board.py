9d569383e220a22e4cecc701c37416b02a06e273
Markus Amano <markus.a.amano@gmail.com>
2020-12-22 15:50:17 -0700

    Added codename_board.py

---
+++ codename_board.py
@@ -0,0 +1,10 @@
+class Board():
+    """
+    This object represents the codename board.
+    """
+    def __init__(self, red = 9 , blue = 8, gray = 7 , black = 1 ):
+        """
+        The initializer of the codename board. It constructs the word_view which 
+        is visible to all players and the spymaster_view. The board is a 5 by 5 board with 25 tiles.
+        """
+        pass
